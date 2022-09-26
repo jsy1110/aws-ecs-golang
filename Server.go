ce2d824aa5238b11094e9870bd96621de3557f49
foreverwin_zzi <jsy11101@gmail.com>
2022-09-26 21:57:47 +0900

    Add files via upload

---
+++ Server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/hello", hello)
+
+	e.GET("/ping", ping)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "hello, world!!")
+}
+
+func ping(c echo.Context) error {
+	return c.String(http.StatusOK, "pong")
+}
